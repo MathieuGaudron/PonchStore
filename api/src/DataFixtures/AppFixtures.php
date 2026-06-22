bce364efb7ca29a72f9540769eb360c78a488ac3
mathieu <mathieu.gaudron@epitech.eu>
2026-06-22 15:04:31 +0200

    [ADD] taux de marge

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -65,30 +65,31 @@ class AppFixtures extends Fixture
         $manager->persist($client);
 
         $produits = [
-            ['Vodka', 'Grey Goose', 'Grey Goose Original', '3024480000128', '6x70cl', '215.00', 42],
-            ['Vodka', 'Absolut', 'Absolut Blue', '7312040017072', '6x70cl', '120.00', 0],
-            ['Vodka', 'Belvedere', 'Belvedere Pure', '5901041003508', '6x70cl', '240.00', 18],
-            ['Rhum', 'Havana Club', 'Havana Club 7 ans', '8501110080705', '6x70cl', '180.00', 35],
-            ['Rhum', 'Bacardi', 'Bacardi Carta Blanca', '5010677012027', '6x100cl', '150.00', 60],
-            ['Rhum', 'Diplomatico', 'Diplomatico Reserva Exclusiva', '7592560000245', '6x70cl', '270.00', 0],
-            ['Rhum', 'Captain Morgan', 'Captain Morgan Spiced Gold', '5000299223031', '6x70cl', '132.00', 24],
-            ['Whisky', 'Jack Daniel\'s', 'Jack Daniel\'s Old No.7', '5099873089798', '6x70cl', '168.00', 50],
-            ['Whisky', 'Jameson', 'Jameson Irish Whiskey', '5011007003234', '6x70cl', '150.00', 12],
-            ['Whisky', 'Chivas', 'Chivas Regal 12 ans', '5000299225011', '6x70cl', '198.00', 0],
-            ['Whisky', 'Glenfiddich', 'Glenfiddich 12 ans', '5010327000138', '6x70cl', '288.00', 8],
-            ['Gin', 'Bombay', 'Bombay Sapphire', '5010677714008', '6x70cl', '144.00', 38],
-            ['Gin', 'Hendrick\'s', 'Hendrick\'s Gin', '5010327755434', '6x70cl', '252.00', 30],
+            ['Vodka', 'Grey Goose', 'Grey Goose Original', '3024480000128', '6x70cl', '167.97', 42, 10],
+            ['Vodka', 'Absolut', 'Absolut Blue', '7312040017072', '6x70cl', '93.75', 0, null],
+            ['Vodka', 'Belvedere', 'Belvedere Pure', '5901041003508', '6x70cl', '187.50', 18, null],
+            ['Rhum', 'Havana Club', 'Havana Club 7 ans', '8501110080705', '6x70cl', '140.63', 35, null],
+            ['Rhum', 'Bacardi', 'Bacardi Carta Blanca', '5010677012027', '6x100cl', '117.19', 60, 10],
+            ['Rhum', 'Diplomatico', 'Diplomatico Reserva Exclusiva', '7592560000245', '6x70cl', '210.94', 0, null],
+            ['Rhum', 'Captain Morgan', 'Captain Morgan Spiced Gold', '5000299223031', '6x70cl', '103.13', 24, null],
+            ['Whisky', 'Jack Daniel\'s', 'Jack Daniel\'s Old No.7', '5099873089798', '6x70cl', '131.25', 50, 8],
+            ['Whisky', 'Jameson', 'Jameson Irish Whiskey', '5011007003234', '6x70cl', '117.19', 12, null],
+            ['Whisky', 'Chivas', 'Chivas Regal 12 ans', '5000299225011', '6x70cl', '154.69', 0, null],
+            ['Whisky', 'Glenfiddich', 'Glenfiddich 12 ans', '5010327000138', '6x70cl', '225.00', 8, null],
+            ['Gin', 'Bombay', 'Bombay Sapphire', '5010677714008', '6x70cl', '112.50', 38, null],
+            ['Gin', 'Hendrick\'s', 'Hendrick\'s Gin', '5010327755434', '6x70cl', '196.88', 30, null],
         ];
 
-        foreach ($produits as [$cat, $marque, $nom, $ean, $format, $prix, $stock]) {
+        foreach ($produits as [$cat, $marque, $nom, $ean, $format, $prixAchat, $stock, $cartonsParPalette]) {
             $produit = new Produit();
             $produit->setNom($nom);
             $produit->setMarque($marque);
             $produit->setDescription($nom . ' - ' . $cat . ' premium, vendu par carton.');
             $produit->setEan($ean);
             $produit->setFormatCarton($format);
-            $produit->setPrixCarton($prix);
+            $produit->setPrixAchatCarton($prixAchat);
             $produit->setStockDisponible($stock);
+            $produit->setCartonsParPalette($cartonsParPalette);
             $produit->setCategorie($categories[$cat]);
             $manager->persist($produit);
         }
